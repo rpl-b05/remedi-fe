55cc3863fd20ac7c17d3b98acbae9f280266e3bc
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2023-12-10 20:09:52 +0700

    feat: footer

---
+++ src/components/elements/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer: React.FC = () => {
+  // TODO: Write element's logic
+
+  return (
+    <footer className="flex justify-center items-center p-4">
+      Made with ❤️ by &nbsp;{' '}
+      <span className="text-teal-500 font-semibold">Remedi</span>
+    </footer>
+  )
+}
