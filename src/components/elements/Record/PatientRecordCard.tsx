c9078431155b09b7c32d8052de30d4cf79a2f56f
nyoosteven <stevenchrist123@gmail.com>
2023-12-11 18:40:21 +0700

    fix:minority

--- src/components/elements/Record/PatientRecordCard.tsx
+++ src/components/elements/Record/PatientRecordCard.tsx
@@ -23,12 +23,16 @@ export const PatientRecordCard = ({
     recordObat,
   } = medicalRecord
   const onVerifyClick = (isVerified: boolean) => {
+    
     try {
-      api.patch(`/record/${id}`, { isVerified })
-      toast.success(
-        `Medical record berhasil di${isVerified ? 'verifikasi' : 'tolak'}`
-      )
-      Router.reload()
+      const patchRecord = async () => {
+        await api.patch(`/record/${id}`, { isVerified })
+        toast.success(
+          `Medical record berhasil di${isVerified ? 'verifikasi' : 'tolak'}`
+        )
+        Router.reload()
+      }
+      patchRecord()
     } catch (err) {
       toast.error(
         `Medical record gagal di${isVerified ? 'verifikasi' : 'tolak'}`
